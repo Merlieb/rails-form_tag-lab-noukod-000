3e60744ab264e7b3c9e3730c37d23148897181d0
Merline Belizaire <merline@noukod.com>
2019-01-28 16:46:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,10 @@ class StudentsController < ApplicationController
 
   end
 
+  def create
+Student.create(first_name: params[:first_name], last_name: params[:last_name])
+redirect_to students_path
+end
   def show
     @student = Student.find(params[:id])
   end
